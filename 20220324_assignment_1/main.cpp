83ea350ffad33ff0c292d0a8dcad4d6967d83fca
tobi1modna <tobia.poppi@virgilio.it>
2022-03-23 19:24:02 +0100

    completed es1

--- 20220324_assignment_1/main.cpp
+++ 20220324_assignment_1/main.cpp
@@ -1,5 +1,7 @@
 #include<fstream>
 #include<vector>
+#include<algorithm>
+#include<map>
 
 int main(int argc, char* argv[]) {
 
@@ -11,6 +13,18 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	std::vector<uint8_t> v;
+	std::vector<uint8_t> v{std::istream_iterator<uint8_t>(is),
+		std::istream_iterator<uint8_t>()};
 
+	std::ofstream os(argv[2]);
+	std::map<uint8_t, int> m;
+
+	for (const uint8_t t : v) {
+		const int counter = std::count(v.cbegin(), v.cend(), t);
+		m.insert(std::pair<uint8_t, int>(t, counter));
+	}
+
+	for (const std::pair<uint8_t, int> t : m) {
+		os << (uint8_t)t.first << '\t' << t.second << '\n';
+	}
 }
