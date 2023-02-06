2ec1622018d91a889ebf685a55e4c05403d5dba5
Tobia Poppi <tobia.poppi@virgilio.it>
2023-02-06 11:36:11 +0100

    Completed packbits, everything working fine.

--- packbits/packbits.cpp
+++ packbits/packbits.cpp
@@ -29,7 +29,6 @@ bool cmp_map_items_asc(std::pair<uint8_t, uint64_t>& a, std::pair<uint8_t, uint6
 	return a.second > b.second;
 }
 
-template<typename T1, typename T2>
 void encode(std::istream& is, std::ostream& os) {
 	uint8_t valcurr;
 	uint8_t valnext;
@@ -55,15 +54,29 @@ void encode(std::istream& is, std::ostream& os) {
 		if (valcurr != valnext) {
 			//copy command
 			v.push_back(valcurr);
-			do {
+			raw_read(is, val2next, 1);
+			is.seekg(-1, std::ios_base::cur);
+
+			while (valcurr != valnext && valnext != val2next && copy_count < 127) {
 				valcurr = valnext;
-				raw_read(is, valnext, 1);
-				raw_read(is, val2next, 1);
 				v.push_back(valcurr);
 				++copy_count;
-				is.seekg(-1, std::ios_base::cur);
-			} while (valcurr != valnext && valnext != val2next && copy_count < 127 && is.peek() != EOF);
 
+				if (is.peek() == EOF) {
+					break;
+				}
+
+				raw_read(is, valnext, 1);
+				if (is.peek() == EOF) {
+					v.push_back(valnext);
+					++copy_count;
+					break;
+				}
+
+				raw_read(is, val2next, 1);
+				is.seekg(-1, std::ios_base::cur);
+			}
+			
 			//add 1 copy command with copy_count , vector
 			raw_write(os, copy_count, 1);
 			for (size_t i = 0; i < copy_count + 1; ++i) {
@@ -74,16 +87,18 @@ void encode(std::istream& is, std::ostream& os) {
 		else {
 			//run command
 			++run_count;
-			while (valcurr == valnext && run_count < 127 && is.peek() != EOF) {
+			while (valcurr == valnext && run_count <= 127) {
+				++run_count;
+				if (is.peek() == EOF) {
+					break;
+				}
 				valcurr = valnext;
 				raw_read(is, valnext, 1);
-				++run_count;
 			}
 			//add 1 copy command with copy_count , vector
 			uint16_t c = 257 - run_count;
 			raw_write(os, c, 1);
 			raw_write(os, valcurr, 1);
-			//size_t pos = is.tellg() - 1;
 			is.seekg(-1, std::ios_base::cur);
 		}
 	}
@@ -91,20 +106,23 @@ void encode(std::istream& is, std::ostream& os) {
 	raw_write(os, e, 1);
 }
 
-template<typename T1, typename T2>
 void decode(std::istream& is, std::ostream& os) {
-	while (is.peek != 128) {
+	while (is.peek() != 128) {
 		uint8_t val;
 		val = is.get();
-		if (1 <= val <= 127) {
+		if (val >= 0 && val <= 127) {
 			uint8_t copy_count = val + 1;
 			for (size_t i = 0; i < copy_count; ++i) {
-
+				uint8_t sym = is.get();
+				os.write(reinterpret_cast<const char*>(&sym), 1);
 			}
 		}
 		else {
 			uint8_t run_count = 257 - val;
-
+			uint8_t sym = is.get();
+			for (size_t i = 0; i < run_count; ++i) {
+				os.write(reinterpret_cast<char*>(&sym), 1);
+			}
 		}
 	}
 	
@@ -128,10 +146,10 @@ int main(int argc, char* argv[]) {
 	std::string mode = argv[1];
 
 	if (mode == "c") {
-		encode<uint8_t, uint64_t>(is, os);
+		encode(is, os);
 	}
 	else if (mode == "d") {
-		decode<uint8_t, uint64_t>(is, os);
+		decode(is, os);
 	}
 	else {
 		error("Syntax error: wrong mode parameter [c|d].");
