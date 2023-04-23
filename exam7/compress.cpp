1e385de3993863472a9143db7fe48485d3633680
tobi1modna <tobia.poppi@virgilio.it>
2023-04-23 17:00:42 +0200

    Solved packbits exercise of exam7, working fine. Next steps: base64 encoding and jsonify.

--- exam7/compress.cpp
+++ exam7/compress.cpp
@@ -4,86 +4,100 @@
 
 void PackBitsEncode(const mat<uint8_t>& img, std::vector<uint8_t>& encoded) {
 	uint8_t curr;
-	uint8_t prev;
-	bool init = true;
-	bool literalling = false;
-	bool running = false;
-	uint8_t cp_count = 1;
+	uint8_t next;
+	uint8_t next_next;
+	uint8_t copy_count;
+	uint8_t run_count;
 	std::vector<uint8_t> lit;
+	size_t n = 0;
+	bool end = false;
 
-	for (size_t i = 0; i < img.rows(); ++i) {
-		for (size_t j = 0; j < img.cols(); ++j) {
-			curr = img(i, j);
+	while (true) {
+		copy_count = 0;
+		run_count = 0;
+		lit.clear();
+		curr = img.data()[n];
 
-			if (init == true) {
-				prev = curr;
-				init = false;
+		//if I'm on the last position of input, I must end with a single literal
+		if (n + 1 == img.size()) {
+			encoded.push_back(0);
+			encoded.push_back(curr);
+			break;
+		}
+
+		next = img.data()[n + 1];
+
+		//if the next position is the last one of input, I must end with a 2 literals of a 2-run.
+		if (n + 2 == img.size()) {
+			if (curr == next) {
+				encoded.push_back(255);
+				encoded.push_back(curr);
 			}
 			else {
-				if (running) {
-					if (curr == prev) {
-						if (cp_count == 128) {
-							encoded.push_back(129);
-							encoded.push_back(curr);
-							cp_count = 1;
-							running = true;
-							literalling = false;
-						}
-						else {
-							++cp_count;
-						}
-					}
-					else {
-						encoded.push_back(257 - cp_count);
-						encoded.push_back(prev);
-						running = false;
-						literalling = true;
-						prev = curr;
-						cp_count = 1;
-					}
-				}
-				else if (literalling) {
-					if (curr != prev) {
-						if (lit.size() < 128) {
-							lit.push_back(prev);
-							prev = curr;
-						}
-						else {
-							encoded.push_back(lit.size() - 1);
-							for (size_t k = 0; k < lit.size(); ++k) {
-								encoded.push_back(lit[k]);
-							}
-							lit.clear();
-							literalling = true;
-							prev = curr;
-						}
-					}
-					else {
-						encoded.push_back(lit.size() - 1);
-						for (size_t k = 0; k < lit.size(); ++k) {
-							encoded.push_back(lit[k]);
-						}
-						lit.clear();
-						literalling = false;
-						running = true;
-						prev = curr;
-						++cp_count;
-					}
-				}
-				else {
-					if (curr == prev) {
-						running = true;
-						literalling = false;
-						++cp_count;
-					}
-					else {
-						literalling = true;
-						running = false;
-						lit.push_back(prev);
-						prev = curr;
-					}
-				}
+				encoded.push_back(1);
+				encoded.push_back(curr);
+				encoded.push_back(next);
 			}
+			break;
+		}
+
+		if (curr != next) {
+			//copy command
+			lit.push_back(curr);
+			++copy_count;
+			next_next = img.data()[n + 2];
+
+			while (curr != next && next != next_next && copy_count < 128) {
+				curr = next;
+				lit.push_back(curr);
+				++n;
+				++copy_count;
+				next = next_next;
+
+				if (n + 2 == img.size()) {
+					//here I should increment copy_count to include next but I send directly copy_count instead of copy_count-1
+					encoded.push_back(copy_count);
+					for (size_t i = 0; i < lit.size(); ++i) {
+						encoded.push_back(lit[i]);
+					}
+					encoded.push_back(next);
+					end = true;
+					break;
+				}
+
+				next_next = img.data()[n + 2];
+			}
+			if (end == true) {
+				break;
+			}
+			encoded.push_back(copy_count - 1);
+			for (size_t i = 0; i < lit.size(); ++i) {
+				encoded.push_back(lit[i]);
+			}
+			++n;
+
+		}
+		else {
+			//run command
+			++run_count;
+			while (curr == next && run_count <= 127) {
+				++run_count;
+				curr = next;
+				++n;
+				if (n + 1 == img.size()) {
+					encoded.push_back(257 - run_count);
+					encoded.push_back(curr);
+					end = true;
+					break;
+				}
+				next = img.data()[n + 1];
+			}
+			if (end == true) {
+				break;
+			}
+			encoded.push_back(257 - run_count);
+			encoded.push_back(curr);
+			++n;
 		}
 	}
 	encoded.push_back(128);
--- exam7/process_ppm.cpp
+++ exam7/process_ppm.cpp
@@ -6,95 +6,8 @@
 
 #include "mat.h"
 #include "ppm.h"
-//#include "compress.cpp"
+#include "compress.cpp"
 
-
-void PackBitsEncode(const mat<uint8_t>& img, std::vector<uint8_t>& encoded) {
-	uint8_t curr;
-	uint8_t prev;
-	bool init = true;
-	bool literalling = false;
-	bool running = false;
-	uint8_t cp_count = 1;
-	std::vector<uint8_t> lit;
-
-	for (size_t i = 0; i < img.rows(); ++i) {
-		for (size_t j = 0; j < img.cols(); ++j) {
-			curr = img(i, j);
-
-			if (init == true) {
-				prev = curr;
-				init = false;
-			}
-			else {
-				if (running) {
-					if (curr == prev) {
-						if (cp_count == 128) {
-							encoded.push_back(129);
-							encoded.push_back(curr);
-							cp_count = 1;
-							running = true;
-							literalling = false;
-						}
-						else {
-							++cp_count;
-						}
-					}
-					else {
-						encoded.push_back(257 - cp_count);
-						encoded.push_back(prev);
-						running = false;
-						literalling = true;
-						prev = curr;
-						cp_count = 1;
-					}
-				}
-				else if (literalling) {
-					if (curr != prev) {
-						if (lit.size() < 128) {
-							lit.push_back(prev);
-							prev = curr;
-						}
-						else {
-							encoded.push_back(lit.size() - 1);
-							for (size_t k = 0; k < lit.size(); ++k) {
-								encoded.push_back(lit[k]);
-							}
-							lit.clear();
-							literalling = true;
-							prev = curr;
-						}
-					}
-					else {
-						encoded.push_back(lit.size() - 1);
-						for (size_t k = 0; k < lit.size(); ++k) {
-							encoded.push_back(lit[k]);
-						}
-						lit.clear();
-						literalling = false;
-						running = true;
-						prev = curr;
-						++cp_count;
-					}
-				}
-				else {
-					if (curr == prev) {
-						running = true;
-						literalling = false;
-						++cp_count;
-					}
-					else {
-						literalling = true;
-						running = false;
-						lit.push_back(prev);
-						prev = curr;
-					}
-				}
-			}
-		}
-	}
-	encoded.push_back(128);
-}
 
 bool LoadPPM(const std::string& filename, mat<vec3b>& img) {
 	std::ifstream is(filename, std::ios::binary);
@@ -150,7 +63,14 @@ int main(int argc, char** argv) {
 
 	SplitRGB(img, img_r, img_g, img_b);
 
+	std::ofstream os(argv[2]);
+
+	for (size_t i = 0; i < img_r.size(); ++i) {
+		os << +img_r.data()[i] << std::endl;
+	}
+
 	std::vector<uint8_t> p;
 
 	PackBitsEncode(img_r, p);
+	uint8_t s = 1;
 }
