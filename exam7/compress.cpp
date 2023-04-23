5e582f480f5f6a36116fcc653f3934a7b46b8972
Tobia Poppi <tobia.poppi@virgilio.it>
2023-04-23 11:45:50 +0200

    packbits encoding is not working

--- exam7/compress.cpp
+++ exam7/compress.cpp
@@ -1,13 +1,6 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <algorithm>
-#include <iterator>
-
+#include <vector>
 #include "mat.h"
 #include "ppm.h"
-//#include "compress.cpp"
-
 
 void PackBitsEncode(const mat<uint8_t>& img, std::vector<uint8_t>& encoded) {
 	uint8_t curr;
@@ -94,63 +87,4 @@ void PackBitsEncode(const mat<uint8_t>& img, std::vector<uint8_t>& encoded) {
 		}
 	}
 	encoded.push_back(128);
-}
-
-bool LoadPPM(const std::string& filename, mat<vec3b>& img) {
-	std::ifstream is(filename, std::ios::binary);
-	if (!is) {
-		std::cout << "Error: cannot open input file." << std::endl;
-		return false;
-	}
-	std::string magic;
-	std::uint32_t width;
-	std::uint32_t height;
-	std::uint32_t maxval;
-	std::getline(is, magic);
-	if (is.peek() == '#') {
-		std::string comment;
-		std::getline(is, comment);
-	}
-	is >> width;
-	is >> height;
-	is >> maxval;
-	{
-		uint8_t consume;
-		consume = is.get();
-	}
-	img.resize(height, width);
-	is.read(img.rawdata(), img.rawsize());
-	return true;
-}
-
-void SplitRGB(const mat<vec3b>& img, mat<uint8_t>& img_r, mat<uint8_t>& img_g, mat<uint8_t>& img_b) {
-	img_r.resize(img.rows(), img.cols());
-	img_g.resize(img.rows(), img.cols());
-	img_b.resize(img.rows(), img.cols());
-	for (size_t i = 0; i < img.rows(); ++i) {
-		for (size_t j = 0; j < img.cols(); ++j) {
-			img_r(i, j) = img(i, j)[0];
-			img_g(i, j) = img(i, j)[1];
-			img_b(i, j) = img(i, j)[2];
-		}
-	}
-}
-
-
-
-
-int main(int argc, char** argv) {
-	bool r;
-	mat<vec3b> img;
-	r = LoadPPM(argv[1], img);
-
-	mat<uint8_t> img_r;
-	mat<uint8_t> img_g;
-	mat<uint8_t> img_b;
-
-	SplitRGB(img, img_r, img_g, img_b);
-
-	std::vector<uint8_t> p;
-
-	PackBitsEncode(img_r, p);
 }
