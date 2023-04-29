1938defc7ef9b7922eb90c4074506499cd117efa
Tobia Poppi <tobia.poppi@virgilio.it>
2023-04-29 09:27:56 +0200

    started YUV4MPEG2, halfway through exercise 1

---
+++ YUV4MPEG2/y4m_gray.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <algorithm>
+#include <string>
+
+//#include "pgm.cpp"
+//#include "ppm.cpp"
+#include "mat.h"
+#include "pgm.h"
+#include "ppm.h"
+#include "types.h"
+#include "utils.h"
+
+bool y4m_extract_gray(const std::string& filename, std::vector<mat<uint8_t>>& frames) {
+	std::ifstream is(filename, std::ios::binary);
+	if (!is) {
+		std::cout << "Error: cannot open input file" << std::endl;
+		return false;
+	}
+	std::string mn;
+	std::string tmp;
+	uint32_t W;
+	uint32_t H;
+	is >> mn;
+	while (is.peek() != 0x0A) {
+		is >> tmp;
+		uint8_t tag = tmp.front();
+		tmp.erase(tmp.begin());
+		if (tag == 'W') {
+			W = std::stoi(tmp);
+		}
+		else if (tag == 'H') {
+			H = std::stoi(tmp);
+		}
+		else if (tag == 'C') {
+			if (tmp != "420jpeg") {
+				std::cout << "Error: program only accepts 420jpeg chroma subsampling";
+				return false;
+			}
+		}
+		else if (tag == 'I') {
+			if (tmp != "p") {
+				std::cout << "Error: program only accepts progressive interlacing";
+				return false;
+			}
+		}
+		else if (tag == 'F') {
+			continue;
+		}
+		else if (tag == 'A') {
+			continue;
+		}
+		else if (tag == 'X') {
+			continue;
+		}
+		else {
+			return false;
+		}
+	}
+	uint8_t consume;
+	consume = is.get();
+
+	while (is.peek() != EOF) {
+		std::string t;
+		std::getline(is, t, ' ');
+		std::cout << t[0] << t[1] << t[2];
+
+		mat<uint8_t> f(H, W);
+		is.read(reinterpret_cast<char*>(f.rawdata()), f.rawsize());
+		is.seekg(((H * W) / 4) * 2, std::ios::cur);
+		frames.push_back(f);
+	}
+	
+	return true;
+}
+
+int main(int argc, char** argv) {
+	std::vector<mat<uint8_t>> frames;
+	bool res;
+	res = y4m_extract_gray(argv[1], frames);
+}
